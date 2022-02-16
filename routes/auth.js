5c1d5097055a6de9df5b98e2151aee394f691a8f
Basil Goldman <basil@inqwise.com>
2022-02-16 19:29:53 +0200

    added 2FA

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,15 @@ const adserve = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config.json');
+const speakeasy = require('speakeasy');
+const QRCode = require("qrcode");
 
 router.post('/register', (req, res) => {
 
   const username = req.body.username;
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 8);
+  const useTwoFactorAuthentication = req.body.use_two_factor_authentication;
 
   if(username && email && password) {
 
@@ -19,15 +22,27 @@ router.post('/register', (req, res) => {
       .get(null, username, null, null)
       .then(user => {
         if(!user) {
+
+          // TODO:
           // Set
           adserve
             .users()
             .set(null, username, email, password, 1)
             .then(data => {
+
+              // TODO: check if user enabled for 2FA
+              // 2FA
+              var secret = speakeasy.generateSecret({
+                name: 'AdServe'
+              });
+
               res
                 .json({
-                  id: data
+                  id: data,
+                  otpauthUrl: secret.otpauth_url,
+                  base32: secret.base32 // TODO: store to user
                 });
+
             })
             .catch(err => {
               res
@@ -96,7 +111,7 @@ router.post('/login', (req, res) => {
             username: user.username,
             email: user.email,
             accessToken: token
-          })
+          });
 
       } else {
         // User not found
@@ -112,6 +127,53 @@ router.post('/login', (req, res) => {
           error: err.message
         });
     });
+
+});
+
+// 2 two-factor authentication
+router.post('/2fa/generate', (req, res) => {
+
+  const otpauthUrl = req.body.otpauth_url
+
+  if(otpauthUrl) {
+    // otpauth://totp/AdServe?secret=OM4DKTSWH5DTAVBTIQ7GWQDBM5FSG6DFNNKSSV2FNRWE4ZB4J55A
+    QRCode.toDataURL(otpauthUrl, function(err, data_url) {
+      console.log(data_url);
+
+      // Display this data URL to the user in an <img> tag
+      // Example:
+      res.end('<img src="' + data_url + '">');
+    });
+  } else {
+    res.json({
+      error: 'otpauth not provided'
+    });
+  }
+
+});
+
+router.post('/2fa/verify', (req, res) => {
+
+  const token = req.body.token;
+  const secret = req.body.secret; // TODO: get from user
+
+  if(token) {
+
+    const tokenValidates = speakeasy.totp.verify({
+      secret: secret,
+      encoding: 'base32',
+      token: token,
+    });
+
+    res.json({
+      verified: tokenValidates
+    });
+
+  } else {
+    res.json({
+      error: 'user token not provided'
+    });
+  }
 
 });
 
