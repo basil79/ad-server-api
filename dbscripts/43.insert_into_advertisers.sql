130104f845bf50ce023e4b671456237859046873
Basil Goldman <basilgoldman@Basils-iMac.local>
2022-04-03 17:38:59 +0300

    insert demand accounts, insert advertisers

---
+++ dbscripts/43.insert_into_advertisers.sql
@@ -0,0 +1,3 @@
+USE `adserve`;
+INSERT INTO advertisers (name, demand_account_id, account_id, is_active) VALUES ('AppNexus (HB)',  3, 1, 1);
+INSERT INTO advertisers (name, demand_account_id, account_id, is_active) VALUES ('Rubicon (HB)',  15, 1, 1);
